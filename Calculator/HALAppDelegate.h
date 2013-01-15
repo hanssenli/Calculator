7d893cfd9fc74f9b833cf3f841f74f0232527300
hanssenli <hanssenli@gmail.com>
2013-01-14 20:20:47 -0500

    Initial Commit

---
+++ Calculator/HALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HALAppDelegate.h
+//  Calculator
+//
+//  Created by Hanssen Li on 1/14/13.
+//  Copyright (c) 2013 Hanssen Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
